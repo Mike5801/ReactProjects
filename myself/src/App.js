2bb7d8197c5ca64c9c55ef85080577a9caf2e1d5
Miguel Weiping Tang Feng <88734115+Mike5801@users.noreply.github.com>
2022-11-28 20:50:59 -0600

    add base navbar

--- myself/src/App.js
+++ myself/src/App.js
@@ -2,33 +2,35 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 import CardList from './components/card-list/card-list.component'
+import SideNavBar from './components/side-navbar/side-navbar.component'
 
 const App = () => {
 
-  const [animeList, setAnimeList] = useState([]);
+  // const [animeList, setAnimeList] = useState([]);
 
-  const list = [
-    {name: "anime 1",
-    desc: "description 1"},
-    {name: "anime 2",
-    desc: "description 2"},
-    {name: "anime 3",
-    desc: "description 3"},
-    {name: "anime 4",
-    desc: "description 4"},
-    {name: "anime 5",
-    desc: "description 5"},
-    {name: "anime 6",
-    desc: "description 6"}
-  ];
+  // const list = [
+  //   {name: "anime 1",
+  //   desc: "description 1"},
+  //   {name: "anime 2",
+  //   desc: "description 2"},
+  //   {name: "anime 3",
+  //   desc: "description 3"},
+  //   {name: "anime 4",
+  //   desc: "description 4"},
+  //   {name: "anime 5",
+  //   desc: "description 5"},
+  //   {name: "anime 6",
+  //   desc: "description 6"}
+  // ];
 
-  useEffect(() => {
-    setAnimeList(list);
-  }, []);
+  // useEffect(() => {
+  //   setAnimeList(list);
+  // }, []);
 
   return (
     <div className="App">
       {/* <CardList list={animeList}/> */}
+      <SideNavBar />
     </div>
   );
 }
---
+++ myself/src/components/side-navbar/side-navbar.component.jsx
@@ -0,0 +1,143 @@
+import './side-navbar.styles.css';
+
+const SideNavBar = () => {
+    return (
+        <div className='sidebar'>
+            <div className="logo-content">
+                <div className="logo">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-code-circle" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                        <path d="M10 14l-2 -2l2 -2"></path>
+                        <path d="M14 10l2 2l-2 2"></path>
+                        <circle cx="12" cy="12" r="9"></circle>
+                    </svg>
+                    <div className="logo-name">CodingLab</div>
+                </div>
+                <svg id="btn" xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-menu-2" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                    <line x1="4" y1="6" x2="20" y2="6"></line>
+                    <line x1="4" y1="12" x2="20" y2="12"></line>
+                    <line x1="4" y1="18" x2="20" y2="18"></line>
+                </svg>
+            </div>
+            <ul className="nav-list">
+                <li className="nav-list-element">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-search" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                        <circle cx="10" cy="10" r="7"></circle>
+                        <line x1="21" y1="21" x2="15" y2="15"></line>
+                    </svg>
+                    <input type="text" placeholder="Search..."/>
+                    {/* <span className="tooltip">Dashboard</span> */}
+                </li>
+                <li className="nav-list-element">
+                    <a href="#">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-layout-grid" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <rect x="4" y="4" width="6" height="6" rx="1"></rect>
+                            <rect x="14" y="4" width="6" height="6" rx="1"></rect>
+                            <rect x="4" y="14" width="6" height="6" rx="1"></rect>
+                            <rect x="14" y="14" width="6" height="6" rx="1"></rect>
+                        </svg>
+                        <span className="links-name">Dashboard</span>
+                    </a>
+                    {/* <span className="tooltip">Dashboard</span> */}
+                </li>
+                <li className="nav-list-element">
+                    <a href="#">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <circle cx="12" cy="7" r="4"></circle>
+                            <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2"></path>
+                        </svg>
+                        <span className="links-name">User</span>
+                    </a>
+                    {/* <span className="tooltip">Dashboard</span> */}
+                </li>
+                <li className="nav-list-element">
+                    <a href="#">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-messages" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <path d="M21 14l-3 -3h-7a1 1 0 0 1 -1 -1v-6a1 1 0 0 1 1 -1h9a1 1 0 0 1 1 1v10"></path>
+                            <path d="M14 15v2a1 1 0 0 1 -1 1h-7l-3 3v-10a1 1 0 0 1 1 -1h2"></path>
+                        </svg>
+                        <span className="links-name">Messages</span>
+                    </a>
+                    {/* <span className="tooltip">Dashboard</span> */}
+                </li>
+                <li className="nav-list-element">
+                    <a href="#">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-chart-pie" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <path d="M10 3.2a9 9 0 1 0 10.8 10.8a1 1 0 0 0 -1 -1h-6.8a2 2 0 0 1 -2 -2v-7a0.9 .9 0 0 0 -1 -.8"></path>
+                            <path d="M15 3.5a9 9 0 0 1 5.5 5.5h-4.5a1 1 0 0 1 -1 -1v-4.5"></path>
+                        </svg>
+                        <span className="links-name">Analytics</span>
+                    </a>
+                    {/* <span className="tooltip">Dashboard</span> */}
+                </li>
+                <li className="nav-list-element">
+                    <a href="#">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-folder" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <path d="M5 4h4l3 3h7a2 2 0 0 1 2 2v8a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-11a2 2 0 0 1 2 -2"></path>
+                        </svg>
+                        <span className="links-name">File Manager</span>                       
+                    </a>
+                    {/* <span className="tooltip">Dashboard</span> */}
+                </li>
+                <li className="nav-list-element">
+                    <a href="#">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-shopping-cart" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <circle cx="6" cy="19" r="2"></circle>
+                            <circle cx="17" cy="19" r="2"></circle>
+                            <path d="M17 17h-11v-14h-2"></path>
+                            <path d="M6 5l14 1l-1 7h-13"></path>
+                        </svg>
+                        <span className="links-name">Order</span>                        
+                    </a>
+                    {/* <span className="tooltip">Dashboard</span> */}
+                </li>
+                <li className="nav-list-element">
+                    <a href="#">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-heart" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428m0 0a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572"></path>
+                        </svg>
+                        <span className="links-name">Saved</span>                        
+                    </a>
+                    {/* <span className="tooltip">Dashboard</span> */}
+                </li>
+                <li className="nav-list-element">
+                    <a href="#">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-settings" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <path d="M10.325 4.317c.426 -1.756 2.924 -1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543 -.94 3.31 .826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756 .426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543 -.826 3.31 -2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756 -2.924 1.756 -3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543 .94 -3.31 -.826 -2.37 -2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756 -.426 -1.756 -2.924 0 -3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94 -1.543 .826 -3.31 2.37 -2.37c1 .608 2.296 .07 2.572 -1.065z"></path>
+                            <circle cx="12" cy="12" r="3"></circle>
+                        </svg>
+                        <span className="links-name">Settings</span>                        
+                    </a>
+                    {/* <span className="tooltip">Dashboard</span> */}
+                </li>
+            </ul>
+            <div className="div profile-content">
+                <div className="profile">
+                    <div className="profile-details">
+                        <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" alt="" />
+                        <div className="name-job">
+                            <div className="name">Name</div>
+                            <div className="job">Job</div>
+                        </div>
+                    </div>
+                    <svg id="log-out" xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-logout" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                        <path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2"></path>
+                        <path d="M7 12h14l-3 -3m0 6l3 -3"></path>
+                    </svg>
+                </div>
+            </div>
+        </div>
+    );
+}
+export default SideNavBar;
