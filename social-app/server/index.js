3f6c64f211f1215c404dd544e31eeaed57c9f3f5
Miguel Weiping Tang Feng <88734115+Mike5801@users.noreply.github.com>
2023-01-22 17:54:15 -0600

    add post functions

--- social-app/server/index.js
+++ social-app/server/index.js
@@ -11,11 +11,13 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 /* CONTROLLER IMPORTS */
+import { createPost } from "./controllers/posts.js";
 import { register } from "./controllers/auth.js";
 
 /* ROUTE IMPORTS */
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
 
 /* MIDDLEWARE IMPORTS */
 import { verifyToken } from "./middleware/auth.js";
@@ -47,10 +49,12 @@ const upload = multer({storage});
 
 /* ROUTES WITH FILES */
 app.post("/auth/register", upload.single("picture"), register);
+app.post("posts", verifyToken, upload.single("picture"), createPost);
 
 /* ROUTES */
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 
 /* MONGOOSE SETUP */
