0621fb3b1c58f2a39da310a4aae0921c5c6229e4
Miguel Weiping Tang Feng <88734115+Mike5801@users.noreply.github.com>
2023-01-23 17:35:02 -0600

    set styling and theme configuration

--- social-app/client/src/state/index.js
+++ social-app/client/src/state/index.js
@@ -21,7 +21,7 @@ export const authSlice = createSlice({
         },
         setLogout: (state) => {
             state.user = null;
-            state.token = nul;
+            state.token = null;
         },
         setFriends: (state, action) => {
             if (state.user) {
